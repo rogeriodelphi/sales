bc85677eab8d769e3d2cb8ecd05f4856e9ca0df8
rogeriodelphi <rogeriodelphi@gmail.com>
2021-04-17 23:59:42 -0300

    Fixed test

--- core/erp/tests.py
+++ core/erp/tests.py
@@ -1,3 +1,17 @@
+from config.wsgi import *
+from core.erp.models import *
+
+data = ['Leche y derivados', 'Carnes, pescados y huevos', 'Patatas, legumbres, frutos secos',
+        'Verduras y Hortalizas', 'Frutas', 'Cereales y derivados, azúcar y dulces',
+        'Grasas, aceite y mantequilla']
+
+for i in data:
+    cat = Category(name=i)
+    cat.save()
+    print('Guardado registro N°{}'.format(cat.id))
+
+
+
 # from core.erp.models import Type
 
 # Listar
