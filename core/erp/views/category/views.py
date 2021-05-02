2534c28d9746a019b51dadaf2c2e300d48429abb
rogeriodelphi <rogeriodelphi@gmail.com>
2021-05-01 23:56:27 -0300

    Using CreateView and creating validations on our form

--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -47,16 +47,16 @@ class CategoryCreateView(CreateView):
     template_name = 'category/create.html'
     success_url = reverse_lazy('erp:category_list')
 
-    # def post(self, request, *args, **kwargs):
-    #     print(request.POST)
-    #     form = CategoryForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         return HttpResponseRedirect(self.success_url)
-    #     self.object = None
-    #     context = self.get_context_data(**kwargs)
-    #     context['form'] = form
-    #     return render(request, self.template_name, context)
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(self.success_url)
+        self.object = None
+        context = self.get_context_data(**kwargs)
+        context['form'] = form
+        return render(request, self.template_name, context)
 
 
 
