c0f280b6ebc537b2cebfbdac708e76dfc6ba7e3c
rogeriodelphi <rogeriodelphi@gmail.com>
2021-05-03 01:01:47 -0300

    Using UpdateView to delete a record on the form.

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from core.erp.views.category.views import *
 
 app_name = 'erp'
@@ -8,5 +7,6 @@ urlpatterns = [
     path('category/list/', CategoryListView.as_view(), name='category_list'),
     path('category/list2/', category_list, name='category_list2'),
     path('category/add/', CategoryCreateView.as_view(), name='category_create'),
-    path('category/update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('category/edit/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
 ]
--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 
 from core.erp.forms import CategoryForm
@@ -111,9 +111,22 @@ class CategoryUpdateView(UpdateView):
         return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
         context['title'] = 'Edición una Categoria'
         context['entity'] = 'Categorias'
         context['list_url'] = reverse_lazy('erp:category_list')
         context['action'] = 'edit'
+        return context
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category/delete.html'
+    success_url = reverse_lazy('erp:category_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminación de una Categoria'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
         return context
